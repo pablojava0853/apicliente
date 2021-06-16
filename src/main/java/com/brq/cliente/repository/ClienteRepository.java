edc5987c7eef27ebd7851283d0a96dbaecbbf2a0
Pablo Gonzalez <pablojava853@outlook.com>
2021-06-16 16:02:46 -0300

    Criação da interface ClienteRepository

---
+++ src/main/java/com/brq/cliente/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.brq.cliente.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brq.cliente.models.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
